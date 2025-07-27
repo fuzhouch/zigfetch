3cc8506b205992c275ff6d4c5f67f5f7c49bdb04
fuzhou.chen <fuzhouch@gmail.com>
2025-07-27 13:19:45 +0800

    [NEW] Add command line option.

---
+++ src/args.zig
@@ -0,0 +1,63 @@
+//! Functions to parse command arguments. We intentionally avoid
+//! using third-party libraries because I want to ensure zigfetch
+//! works without the needs of downloading any dependency.
+
+const std = @import("std");
+
+pub fn printUsage() void {
+    const usage =
+        \\Usage: zigfetch [options] <url>
+        \\Usage: zigfetch [options] <path>
+        \\
+        \\    Copy a package to one of the following:
+        \\    <url> must point to one of the following:
+        \\    - A git+http / git+https server for the package
+        \\    - A tarball file (with or without compression)
+        \\      containing package source
+        \\    - A git bundle file containing package source
+        \\Examples:
+        \\
+        \\zigfetch --save git+https://example.com/andrewk/fun-example-tool.git
+        \\zigfetch --save https://example.com/andrewk/fun-example-tool/archive/refs/heads/master.tar.gz
+        \\
+        \\Options:
+        \\ -h, --help                   Print this help and exit
+        \\ --global-cache-dir [path]    Override path to global Zig directory
+        \\ --debug-hash                 Print verbose hash information to stdout
+        \\ --save                       Add the fetched package to build.zig.zon
+        \\ --save=[name]                Add the fetched package to build.zig.zon as name
+        \\ --save-exact                 Add the fetched package to build.zig.zon, storing the URL verbatim
+        \\ --save-exact=[name]          Add the fetched package to build.zig.zon as name, storing the URL verbatim
+    ;
+    const stdout_fd = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_fd);
+    const stdout = bw.writer();
+    _ = stdout.print("{s}", .{usage}) catch unreachable;
+    bw.flush() catch unreachable;
+}
+
+const Command = enum {
+    unknown,
+    help,
+    save,
+    save_exact,
+};
+
+const Action = union(Command) {
+    unknown: void,
+    help: void,
+    save: ?[]u8,
+    save_exact: ?[]u8,
+};
+
+pub fn parse(args: [][*:0]u8) !Action {
+    for (args[1..]) |arg| {
+        const argStr: []const u8 = std.mem.span(arg);
+        if (std.mem.eql(u8, argStr, "-h")) {
+            return .help;
+        } else if (std.mem.eql(u8, argStr, "--help")) {
+            return .help;
+        }
+    }
+    return .unknown;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,31 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
+const args = @import("./args.zig");
+// This is to mimic the behavior of zig fetch in 0.14.*.
+// -h, --help
+// --global-cache-dir [path]
+// --debug-hash
+// --save
+// --save=[name]
+// --save-exact
+// --save-exact=[name]
+pub fn main() !void {
+    const action = args.parse(std.os.argv) catch |err| {
+        std.debug.print("Error: {}", .{err});
+    };
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zigfetch_lib");
+    switch (action) {
+        .unknown => {
+            const stdout_fd = std.io.getStdOut().writer();
+            var bw = std.io.bufferedWriter(stdout_fd);
+            const stdout = bw.writer();
+            _ = stdout.print("{s}", .{"Unsupported option\n"}) catch unreachable;
+            bw.flush() catch unreachable;
+        },
+        .help => {
+            args.printUsage();
+            return;
+        },
+        .save => unreachable,
+        .save_exact => unreachable,
+    }
+}
